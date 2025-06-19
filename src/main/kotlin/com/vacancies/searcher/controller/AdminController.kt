0443a9baa62cfc4b70d4b734c3ec71141e992af7
Nmikh <nmikhaluk@gmail.com>
2025-06-19 16:36:16 +0300

    - fixed getLast job

--- src/main/kotlin/com/vacancies/searcher/controller/AdminController.kt
+++ src/main/kotlin/com/vacancies/searcher/controller/AdminController.kt
@@ -1,6 +1,7 @@
 package com.vacancies.searcher.controller
 
 import com.vacancies.searcher.repository.ScraperJobRepository
+import com.vacancies.searcher.repository.VacancyRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/scrapper/admin")
 class AdminController(
-    private val jobRepository: ScraperJobRepository
+    private val jobRepository: ScraperJobRepository,
+    private val vacancyRepository: VacancyRepository
 ) {
     @DeleteMapping("/jobs/all")
     fun removeAllJobs(): ResponseEntity<Unit> {
         jobRepository.deleteAll()
+        vacancyRepository.deleteAll()
         return ResponseEntity.ok().build()
     }
 }
