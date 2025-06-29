eb1e9c83c8863f79ff42fce09abb45ca094182a5
Nmikh <nmikhaluk@gmail.com>
2025-06-29 20:27:58 +0300

    - added vacancies export endpoint

--- src/main/kotlin/com/vacancies/searcher/controller/AdminController.kt
+++ src/main/kotlin/com/vacancies/searcher/controller/AdminController.kt
@@ -1,10 +1,16 @@
 package com.vacancies.searcher.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.vacancies.searcher.model.VacancySource
 import com.vacancies.searcher.repository.ScraperJobRepository
 import com.vacancies.searcher.repository.VacancyRepository
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/scrapper/admin")
 class AdminController(
     private val jobRepository: ScraperJobRepository,
-    private val vacancyRepository: VacancyRepository
+    private val vacancyRepository: VacancyRepository,
+    private val mapper: ObjectMapper
 ) {
     @DeleteMapping("/jobs/all")
     fun removeAllJobs(): ResponseEntity<Unit> {
@@ -31,5 +38,18 @@ class AdminController(
             )
         )
         return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/vacancies/export", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun exportVacancies(): ResponseEntity<Resource> {
+        val vacancies = vacancyRepository.findAll()
+        val jsonBytes = mapper.writeValueAsBytes(vacancies)
+        val resource = ByteArrayResource(jsonBytes)
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vacancies.json")
+            .contentType(MediaType.APPLICATION_JSON)
+            .contentLength(jsonBytes.size.toLong())
+            .body(resource)
     }
 }
